d6ea9b6794a04ddb6acfef4f07be814031491b6b
Mark J. Becker <herzog31@users.noreply.github.com>
2023-02-01 11:22:23 +0100

    Revamp Wilson Blog project (#14)

--- blocks/blog-index/blog-index.js
+++ blocks/blog-index/blog-index.js
@@ -1,49 +1,105 @@
 import { fetchIndex } from '../../scripts/scripts.js';
-import { readBlockConfig, createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
-  const index = `${cfg.type}-index`;
+  const { category: categoryFilter, sport: sportFilter } = cfg;
 
   block.textContent = '';
 
   // Fetch items from index
-  const indexContent = await fetchIndex(index);
+  const indexContent = await fetchIndex('blogpost-index');
+  const blogPosts = indexContent.data
+    // Parse category and sport
+    .map((item) => ({
+      ...item,
+      category: JSON.parse(item.category),
+      sport: JSON.parse(item.sport),
+    }))
+    // Filter by category and sport
+    .filter(({ sport, category }) => {
+      if (categoryFilter
+      && !category.some((el) => el.toLowerCase() === categoryFilter.toLowerCase())) {
+        return false;
+      }
+      if (sportFilter
+      && !sport.some((el) => el.toLowerCase() === sportFilter.toLowerCase())) {
+        return false;
+      }
+      return true;
+    });
 
+  // Show empty message if there are no blog items
+  if (blogPosts.length === 0) {
+    const emptyMessage = document.createRange().createContextualFragment(`<div class="blog-grid empty">
+      <div class="empty">
+        <h2>DELAY OF GAME</h2>
+        <p>Sorry, we don't have anything in this section yet. But check back soon – our roster of stories is growing all the time.</p>
+      </div>
+    </div>`);
+    block.appendChild(emptyMessage);
+    return;
+  }
+
+  const blogHero = document.createRange().createContextualFragment('<div class="blog-feature-container"></div>');
   const blogGrid = document.createRange().createContextualFragment('<div class="blog-grid"></div>');
 
-  indexContent.data.forEach((item, key) => {
-    if (key > 0) {
-      const { pathname } = new URL(item.thumbnail, window.location.href);
-      const sport = item.sport.replace(/\[|\]/g, '').replace(/"/g, '');
-      const desc = `${item.description.substring(0, 90)}...`;
-      const card = document.createRange().createContextualFragment(`<div class="grid-item">
-      <article class="blog-post">
-        <a class="blog-teaser-link" href="${item.path}">
-          <div class="blog-teaser-image">
-          <picture>
-              <source type="image/webp" srcset="${pathname}?width=158&format=webply&optimize=medium">
-              <img src="${pathname}?width=158&format=png&optimize=medium" alt="wilson-blog-post" loading="lazy">
-              </picture>
-          </div>
-          <div class="blog-category">
-            ${sport}
-          </div>
-          <div class="blog-teaser-title">
-            ${item.title}
-          </div>
-          
-        </a>
-        <div class="blog-teaser-description">
-            ${desc}
+  // Limit to 10 items
+  blogPosts.slice(0, 10).forEach((item, key) => {
+    const {
+      image, path, title, description, sport, category, thumbnail,
+    } = item;
+
+    const firstSport = sport.length > 0 ? sport[0] : '';
+
+    // Display first blog post as feature
+    if (key === 0) {
+      const firstCategory = category.length > 0 ? category[0] : '';
+      const imageSrc = new URL(image, window.location.href).pathname;
+
+      const card = document.createRange().createContextualFragment(`
+      <div class="blog-img">
+        <picture>
+          <source type="image/webp" srcset="${imageSrc}?width=1050&format=webply&optimize=medium">
+          <img src="${imageSrc}?width=1050&format=png&optimize=medium">
+        </picture>
+      </div>
+      <div class="blog-text">
+        <div class="blog-tag">
+          <div class="blog-sport">${firstSport} </div>
+          <span class="divider">/</span>
+          <div class="blog-category">${firstCategory} </div>
         </div>
-        </article>
+        <div class="blog-title">${title}</div>
+        <div class="blog-desc">${description}</div>
+        <a class="blog-link" href="${path}">READ ARTICLE</a>
       </div>`);
 
-      blogGrid.firstChild.appendChild(card);
+      blogHero.firstChild.append(card);
+      return;
     }
+
+    const thumbnailSrc = new URL(thumbnail, window.location.href).pathname;
+
+    const card = document.createRange().createContextualFragment(`<div class="grid-item">
+      <article class="blog-post">
+        <a class="blog-teaser-link" href="${path}">
+          <div class="blog-teaser-image">
+            <picture>
+              <source type="image/webp" srcset="${thumbnailSrc}?width=344&format=webply&optimize=medium">
+              <img src="${thumbnailSrc}?width=344&format=png&optimize=medium" alt="wilson-blog-post" loading="lazy">
+            </picture>
+          </div>
+          <div class="blog-category">${firstSport}</div>
+          <div class="blog-teaser-title">${title}</div>
+        </a>
+        <div class="blog-teaser-description">${description}</div>
+      </article>
+    </div>`);
+
+    blogGrid.firstChild.appendChild(card);
   });
 
+  block.appendChild(blogHero);
   block.appendChild(blogGrid);
-  block.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
 }
